bfbb0f82471c77d472c3a03602e83d1775f25434
gaymaz19 <91728033+gaymaz19@users.noreply.github.com>
2021-10-01 09:55:01 +0000

    done with first assignment

---
+++ main.py
@@ -0,0 +1,9 @@
+
+num_x = int(input("enter number x: "))
+num_y = int(input("enter number y: "))
+power_result = num_x ** num_y 
+log_result = int(num_x ** 0.5)
+
+print("x**y: ", power_result)
+print("log(x):", log_result)
+print("ID:", 71528)
